9d66f50766c1eaeff6824fa6ab1f9c5af7997cd3
2karis <gitauk2@gmail.com>
2024-01-13 00:30:24 -0500

    added basic ui to retrieve employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'EmployeeManager-ui';
+export class AppComponent implements OnInit{
+  public employees : Employee[];
+
+  constructor(private employeeService : EmployeeService){
+    this.employees = [];
+  }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees=response;
+        console.log(this.employees)
+      },
+      (error :HttpErrorResponse)=>{
+        alert(error.message)
+      }
+    )
+  }
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,22 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private appServerUrl = ''
+  private appServerUrl = environment.appBaseUrl;
 
   constructor( private http: HttpClient) { }
 
   public getEmployees():Observable<Employee[]>{
-    return this.http.get<Employee[]>(`${this.appServerUrl}/employee/all`)
+    return this.http.get<Employee[]>(`${this.appServerUrl}/employee/readall`)
   }
 
   public getEmployee(id : number):Observable<Employee>{
-    return this.http.get<Employee>(`${this.appServerUrl}/employee/get/${id}`)
+    return this.http.get<Employee>(`${this.appServerUrl}/employee/read/${id}`)
   }
   public addEmployee(empolyee : Employee):Observable<Employee>{
     return this.http.post<Employee>(`${this.appServerUrl}/employee/create`, empolyee)
